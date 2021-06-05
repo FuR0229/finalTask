a9a42c949bb7667254951756a3f0c383f8879302
FuR0229 <470533173@qq.com>
2021-06-05 16:08:57 +0800

    显示消息

--- chatroom/src/main/java/chat/controller/messageController.java
+++ chatroom/src/main/java/chat/controller/messageController.java
@@ -1,11 +1,12 @@
 package chat.controller;
 
 import chat.Service.messageService;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jms.JMSException;
+import javax.jms.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -31,12 +32,31 @@ public class messageController {
 
     @ResponseBody
     @RequestMapping(value = "/getResult",method = RequestMethod.POST)
-    public void getResult(@RequestParam String result){
+    public Object getResult(@RequestParam String result,HttpSession session,HttpServletResponse response) throws IOException {
         this.room=result;
+        System.out.println(session.getAttribute("name") + "加入了"+this.room);
+        object.put("msg","ok");
+        return object;
     }
+
     @ResponseBody
-    @RequestMapping(value = "/send",method = RequestMethod.POST)
-    public String send(HttpServletRequest request,HttpServletResponse response , HttpSession session) throws JMSException, IOException {
+    @RequestMapping(value = "/send",method=RequestMethod.POST)
+    public Object send(@RequestParam String message, HttpServletRequest request, HttpSession session)  {
+        //String message = request.getParameter("input");
+        Calendar calendar = Calendar.getInstance(); // gets current instance of the calendar
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        message=formatter.format(calendar.getTime())+" :"+message;
+        producer.sendMessage(this.room,message);
+        System.out.println(session.getAttribute("name") + "  "+message);
+        object.put("msg","ok");
+        //response.getWriter().write(String.valueOf(object));
+        //return "<script language=\"javascript\">window.location.href=\"/chat\"</script>";
+        return object;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/sendFile",method = RequestMethod.POST)
+    public String sendFile(HttpServletRequest request,HttpServletResponse response , HttpSession session) throws JMSException, IOException {
         String message = request.getParameter("input");
         Calendar calendar = Calendar.getInstance(); // gets current instance of the calendar
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -48,5 +68,29 @@ public class messageController {
         //response.getWriter().write(String.valueOf(object));
         //return "<script language=\"javascript\">window.location.href=\"/chat\"</script>";
         return producer.receiveMessage(room);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/receive",method = RequestMethod.GET)
+    public String receive(HttpSession session) throws JMSException{
+        /*ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        final Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        final Destination destination = session.createQueue(room);
+        MessageConsumer consumer = session.createConsumer(destination);
+        consumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                try {
+                    if (message instanceof TextMessage) {
+                        TextMessage textMessage = (TextMessage) message;
+                        System.out.println(textMessage.getText());
+                    }
+                }catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
+        return session.getAttribute("name") + "  "+producer.receiveMessage(room);
     }
 }
